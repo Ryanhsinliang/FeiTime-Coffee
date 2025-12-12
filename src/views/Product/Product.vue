8f0c2c8047c70598f6f2caf2251ddeb6b2eda313
Inai <aromr990426@gmail.com>
2025-12-12 23:28:45 +0800

    style : make new filter1
    style : making filter hover 效果
    
    style : add komori 效果
    
    style : new filter done
    
    style : make filter more good

--- src/views/Product/Product.vue
+++ src/views/Product/Product.vue
@@ -3,10 +3,54 @@
     <div class="mt-[100px] mb-[80px] mx-[3%] flex flex-col items-center">
       <input
         class="border-2 border-solid border-[var(--soft-brown)] lg:w-[70%] lg:text-[24px] lg:py-[12px] lg:px-[24px] lg:rounded-[12px] md:w-[70%] md:text-[20px] md:py-[8px] md:px-[24px] md:rounded-[12px] w-[88%] text-[20px] py-[8px] px-[18px] rounded-[8px]"
-        type="text"
+        type="search"
         placeholder="喝一杯靜謐的午後時光"
       />
+      <div class="filter">
+        <div class="filter-type">
+          <p class="filter-word" tabindex="0">類別</p>
+          <div class="type-list">
+            <p class="list" tabindex="0">咖啡豆</p>
+            <p class="list" tabindex="0">濾掛包</p>
+            <p class="list" tabindex="0">器具</p>
+          </div>
+        </div>
+        <div class="filter-type">
+          <p class="filter-word" tabindex="0">焙度</p>
+          <div class="type-list">
+            <p class="list" tabindex="0">淺焙</p>
+            <p class="list" tabindex="0">中焙</p>
+            <p class="list" tabindex="0">深焙</p>
+          </div>
+        </div>
+        <div class="filter-type">
+          <p class="filter-word" tabindex="0">風格</p>
+          <div class="type-list">
+            <p class="list" tabindex="0">果香明亮</p>
+            <p class="list" tabindex="0">清爽茶感</p>
+            <p class="list" tabindex="0">甜感平衡</p>
+            <p class="list" tabindex="0">可可堅果</p>
+            <p class="list" tabindex="0">濃厚飽滿</p>
+            <p class="list" tabindex="0">酒香發酵</p>
+          </div>
+        </div>
+        <div class="filter-type">
+          <p class="filter-word" tabindex="0">產地</p>
+          <div class="type-list">
+            <p class="list" tabindex="0">肯亞</p>
+            <p class="list" tabindex="0">哥倫比亞</p>
+            <p class="list" tabindex="0">瓜地馬拉</p>
+            <p class="list" tabindex="0">巴西</p>
+            <p class="list" tabindex="0">哥斯大黎加</p>
+            <p class="list" tabindex="0">印尼</p>
+            <p class="list" tabindex="0">薩爾瓦多</p>
+            <p class="list" tabindex="0">衣索比亞</p>
+          </div>
+        </div>
+      </div>
     </div>
+
+    <!-- 【排序】 價錢 熱門度 甜味 酸味 口感 餘韻 澄澈度 -->
 
     <div
       class="grid lg:grid-cols-3 lg:mx-[3%] lg:w-[94%] lg:gap-[80px] md:mx-[6%] md:w-[88%] md:gap-[60px] md:grid-cols-2 mx-[6%] w-[88%] gap-[60px] grid-cols-1"
@@ -199,6 +243,8 @@
     /* 抹茶綠 */
     --soft-brown: #dccfc0;
     /* 亮咖啡 */
+    --heavy-brown: #b49e89;
+    /* 深咖啡 */
     --light-gray: #eeeeee;
     /* 淡灰 */
   }
@@ -223,88 +269,66 @@
     box-shadow: none;
   }
 
-  /* .disabled-word {
-  color: var(--light-gray);
-} */
-  /* color-[var(--light-gray)] */
-
-  /* .product-serve {
-    margin-top: 100px;
-    margin-bottom: 80px;
-    margin-left: 3%;
-    margin-right: 3%;
+  .filter {
     display: flex;
-    flex-direction: column;
-    align-items: center;
-  } */
-
-  /* .product-serve {
-  @apply mt-[100px] mb-[80px] mx-[3%] flex flex-col items-center;
-} */
-
-  /* .find {
-    border: 2px solid var(--green-gray);
+    padding: 0 10px;
+    padding-top: 12px;
     width: 70%;
-    font-size: 24px;
-    padding: 12px 24px;
+  }
+
+  .filter-type {
+    font-size: 20px;
+    padding: 4px 12px;
+    background-color: var(--soft-brown);
     border-radius: 12px;
-  } */
-
-  /* .find {
-  @apply border-2 border-solid border-[var(--green-gray)] w-[70%] text-[24px] py-[12px] px-[24px] rounded-[12px];
-} */
-
-  /* .product-pic-area {
-    display: grid;
-    margin: 0 3%;
-    width: 94%;
-    gap: 80px;
-    grid-template-columns: repeat(3, 1fr);
-  } */
-  /* .product-pic-area {
-  @apply grid grid-cols-3 mx-[3%] w-[94%] gap-[80px];
-} */
-
-  /* .product-card {
+    margin-left: 8px;
+    margin-right: 8px;
     position: relative;
-  } */
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
 
-  /* .product-card {
-  @apply relative;
-} */
+  .type-list {
+    display: none;
+  }
 
-  /* .product-pic {
-    width: 100%;
-  } */
+  .filter-type:focus-within {
+    background-color: var(--heavy-brown);
+  }
 
-  /* .product-pic {
-  @apply w-[100%];
-} */
-
-  /* .product-word {
+  .filter-type:focus-within .type-list {
     display: flex;
     flex-direction: column;
-    align-items: center;
+    background-color: var(--soft-brown);
+    border-radius: 16px;
     position: absolute;
-    width: 100%;
-    bottom: 24px;
+    top: calc(100% + 12px);
     left: 50%;
     transform: translateX(-50%);
-    font-size: 20px;
-  } */
+    white-space: nowrap;
+    padding: 0px 24px;
+    z-index: 2;
+  }
 
-  /* .product-word {
-  @apply flex flex-col items-center absolute w-[100%] bottom-[24px] left-[50%] text-[20px] -translate-x-[50%];
-} */
+  .list {
+    margin: 12px;
+    padding: 12px 24px;
+    border-bottom: 2px solid transparent;
+  }
 
-  /* .product-name {
-    font-size: 28px;
-    font-weight: 700;
-  } */
+  .list:hover {
+    border-bottom: 2px solid var(--heavy-brown);
+    color: var(--heavy-brown);
+  }
 
-  /* .product-name {
-  @apply text-[28px] font-bold;
-} */
+  .list:focus {
+    background-color: var(--heavy-brown);
+    border-bottom: 2px solid transparent;
+    color: #222222;
+    border-radius: 16px;
+    box-shadow: 0 4px 4px 2px rgba(0, 0, 0, 0.35);
+  }
 
   @media (768px <= width < 1024px) {
   }

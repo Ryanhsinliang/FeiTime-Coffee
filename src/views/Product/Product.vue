417e8cb0b0a11afdd6ac92d4b19d24c98f207359
kitty <Kitty@kitty.com>
2025-12-18 09:35:44 +0800

    style : top-bar can fly

--- src/views/Product/Product.vue
+++ src/views/Product/Product.vue
@@ -1,80 +1,84 @@
 <template>
   <div class="font-wenkai text-[#222222]">
-    <div class="mt-[100px] mb-[80px] mx-[3%] flex flex-col items-center">
-      <input
-        class="border-2 border-solid border-[var(--soft-brown)] lg:w-[70%] lg:text-[24px] lg:py-[12px] lg:px-[24px] lg:rounded-[12px] md:w-[80%] md:text-[20px] md:py-[8px] md:px-[24px] md:rounded-[12px] w-[94%] text-[20px] py-[8px] px-[18px] rounded-[8px]"
-        type="search"
-        placeholder="喝一杯靜謐的午後時光"
-      />
-      <div class="filter">
-        <div class="filter-type">
-          <p class="filter-word" tabindex="0">類別</p>
-          <div class="type-list">
-            <p class="list" tabindex="0">咖啡豆</p>
-            <p class="list" tabindex="0">濾掛包</p>
-            <p class="list" tabindex="0">器具</p>
-          </div>
-        </div>
-        <div class="filter-type">
-          <p class="filter-word" tabindex="0">焙度</p>
-          <div class="type-list">
-            <p class="list" tabindex="0">淺焙</p>
-            <p class="list" tabindex="0">中焙</p>
-            <p class="list" tabindex="0">深焙</p>
-          </div>
-        </div>
-        <div class="filter-type">
-          <p class="filter-word" tabindex="0">風格</p>
-          <div class="type-list">
-            <p class="list" tabindex="0">果香明亮</p>
-            <p class="list" tabindex="0">清爽茶感</p>
-            <p class="list" tabindex="0">甜感平衡</p>
-            <p class="list" tabindex="0">可可堅果</p>
-            <p class="list" tabindex="0">濃厚飽滿</p>
-            <p class="list" tabindex="0">酒香發酵</p>
-          </div>
-        </div>
-        <div class="filter-type">
-          <p class="filter-word" tabindex="0">產地</p>
-          <div class="type-list">
-            <p class="list" tabindex="0">肯亞</p>
-            <p class="list" tabindex="0">哥倫比亞</p>
-            <p class="list" tabindex="0">瓜地馬拉</p>
-            <p class="list" tabindex="0">巴西</p>
-            <p class="list" tabindex="0">哥斯大黎加</p>
-            <p class="list" tabindex="0">印尼</p>
-            <p class="list" tabindex="0">薩爾瓦多</p>
-            <p class="list" tabindex="0">衣索比亞</p>
-          </div>
-        </div>
+    <div class="top-find-bar">
+      <div class="mb-[12px] mx-[3%] flex flex-col items-center">
+        <input
+          class="border-2 border-solid border-[var(--soft-brown)] lg:w-[70%] lg:text-[24px] lg:py-[12px] lg:px-[24px] lg:rounded-[12px] md:w-[80%] md:text-[20px] md:py-[8px] md:px-[24px] md:rounded-[12px] w-[94%] text-[20px] py-[8px] px-[18px] rounded-[8px]"
+          type="search"
+          placeholder="喝一杯靜謐的午後時光"
+        />
       </div>
-    </div>
-    <div class="sort">
-      <img class="IC-sort" src="./assets/sort.svg" alt="" />
-      <select id="sort-page">
-        <option value="">排序</option>
-        <option value="">價錢</option>
-        <option value="">熱門度</option>
-        <option value="">甜味</option>
-        <option value="">酸味</option>
-        <option value="">口感</option>
-        <option value="">餘韻</option>
-        <option value="">澄澈度</option>
-      </select>
-      <div class="line-hako">
-        <div class="line">
-          <label for="circle">
-            <input type="checkbox" id="circle" />
-            <div class="maru" @click="sortChange"></div>
-          </label>
-          <span v-if="sortHe" class="oriru">↓高到低</span>
-          <span v-else class="oriru">↑低到高</span>
+      <div class="find">
+        <div class="filter">
+          <div class="filter-type">
+            <p class="filter-word" tabindex="0">類別</p>
+            <div class="type-list">
+              <p class="list" tabindex="0">咖啡豆</p>
+              <p class="list" tabindex="0">濾掛包</p>
+              <p class="list" tabindex="0">器具</p>
+            </div>
+          </div>
+          <div class="filter-type">
+            <p class="filter-word" tabindex="0">焙度</p>
+            <div class="type-list">
+              <p class="list" tabindex="0">淺焙</p>
+              <p class="list" tabindex="0">中焙</p>
+              <p class="list" tabindex="0">深焙</p>
+            </div>
+          </div>
+          <div class="filter-type">
+            <p class="filter-word" tabindex="0">風格</p>
+            <div class="type-list">
+              <p class="list" tabindex="0">果香明亮</p>
+              <p class="list" tabindex="0">清爽茶感</p>
+              <p class="list" tabindex="0">甜感平衡</p>
+              <p class="list" tabindex="0">可可堅果</p>
+              <p class="list" tabindex="0">濃厚飽滿</p>
+              <p class="list" tabindex="0">酒香發酵</p>
+            </div>
+          </div>
+          <div class="filter-type">
+            <p class="filter-word" tabindex="0">產地</p>
+            <div class="type-list">
+              <p class="list" tabindex="0">肯亞</p>
+              <p class="list" tabindex="0">哥倫比亞</p>
+              <p class="list" tabindex="0">瓜地馬拉</p>
+              <p class="list" tabindex="0">巴西</p>
+              <p class="list" tabindex="0">哥斯大黎加</p>
+              <p class="list" tabindex="0">印尼</p>
+              <p class="list" tabindex="0">薩爾瓦多</p>
+              <p class="list" tabindex="0">衣索比亞</p>
+            </div>
+          </div>
+        </div>
+        <div class="sort">
+          <img class="IC-sort" src="./assets/sort.svg" alt="" />
+          <select id="sort-page">
+            <option value="">排序</option>
+            <option value="">價錢</option>
+            <option value="">熱門度</option>
+            <option value="">甜味</option>
+            <option value="">酸味</option>
+            <option value="">口感</option>
+            <option value="">餘韻</option>
+            <option value="">澄澈度</option>
+          </select>
+          <div class="line-hako">
+            <div class="line">
+              <label for="circle">
+                <input type="checkbox" id="circle" />
+                <div class="maru" @click="sortChange"></div>
+              </label>
+              <span v-if="sortHe" class="oriru">↓高到低</span>
+              <span v-else class="oriru">↑低到高</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
 
     <div
-      class="grid lg:grid-cols-3 lg:mx-[3%] lg:w-[94%] lg:gap-[80px] md:mx-[6%] md:w-[88%] md:gap-[60px] md:grid-cols-2 mx-[6%] w-[88%] gap-[60px] grid-cols-1"
+      class="grid lg:grid-cols-3 lg:mx-[3%] lg:w-[94%] lg:gap-[80px] md:mx-[6%] md:w-[88%] md:gap-[60px] md:grid-cols-2 mx-[6%] w-[88%] gap-[60px] grid-cols-1 pt-[184px]"
     >
       <!-- card start -->
       <a href="#" target="_blank">
@@ -309,10 +313,28 @@
     box-shadow: none;
   }
 
+  .top-find-bar {
+    background-color: #73cf41;
+    padding-top: 40px;
+    padding-bottom: 24px;
+    width: 100%;
+    position: fixed;
+    z-index: 2;
+  }
+
+  .find {
+    margin-left: 16.95%;
+    margin-right: 16.95%;
+    width: 70%;
+    display: flex;
+    white-space: nowrap;
+    /* justify-content: space-between; */
+    align-items: center;
+  }
+
   .filter {
     display: flex;
     padding: 0 16px;
-    padding-top: 12px;
     width: 70%;
   }
 

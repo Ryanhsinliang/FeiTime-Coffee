ee060f718cd3ce7f7c7d6c4222aa0c0f24c73308
RyanKuo <t5a2r0@gmail.com>
2026-01-08 01:29:04 +0800

    fix: 註冊頁及重發驗證信切版及路由修正

--- src/views/Register/EmailComfirmed.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container">
-    <h1>驗證成功！ 🎉</h1>
-    <p>您的帳號已經可以正常使用，請點擊下方按鈕進行登入。</p>
-    <router-link to="/login">
-      <button>前往登入</button>
-    </router-link>
-  </div>
-</template>

246caf83c7e4c892124f2d739f15faa5ee2a9d91
Inai <aromr990426@gmail.com>
2026-01-10 22:28:06 +0800

    style: 成功頁完成

--- src/views/PaySuccess.vue
+++ src/views/PaySuccess.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 打勾 -->
-  <div class="flex flex-col items-center my-[64px]">
+  <div class="flex flex-col items-center my-[64px] w-[450px] mx-auto">
     <div
       class="rounded-full bg-[var(--green-gray)] inline-block w-[100px] h-[100px] text-center leading-[100px] text-[24px] text-white shadow-[0_0_20px_20px_rgba(0,0,0,0.1)]"
     >
@@ -11,31 +11,33 @@
     <p class="text-[#666666]">感謝您的購買！FeiTime期待您再次光臨</p>
 
     <!-- 訂單資訊 -->
-    <div class="bg-white rounded-[12px] border-[#dddddd] border-4 text-[20px] my-[32px]">
-      <div class="flex">
-        <p>訂單編號</p>
-        <p>{{ '🩷請串訂單編號' }}</p>
+    <div
+      class="bg-white rounded-[12px] border-[#dddddd] border-4 text-[20px] my-[32px] w-[100%] px-[24px] py-[24px]"
+    >
+      <div class="flex justify-between">
+        <p class="text-[#666666]">訂單編號</p>
+        <p class="text-[--heavy-brown]">6658876876465454656</p>
       </div>
-      <div class="flex">
-        <p>總金額</p>
-        <p>{{ '🩷請串amount' }}</p>
+      <div class="flex justify-between my-[16px]">
+        <p class="text-[#666666]">總金額</p>
+        <p class="text-[--heavy-brown]">NT$ 7890</p>
       </div>
-      <div class="flex">
-        <p>購買日期</p>
-        <p>{{ '🩷請串日期' }}</p>
+      <div class="flex justify-between">
+        <p class="text-[#666666]">購買日期</p>
+        <p class="text-[--heavy-brown]">2065/10/3 18:16</p>
       </div>
     </div>
 
     <router-link
       to="/product"
-      class="text-xl bg-[var(--green-gray)] px-[48px] py-[24px] rounded-full text-[var(--main-color)] font-[600]"
+      class="text-xl bg-[var(--green-gray)] px-[48px] py-[20px] rounded-full text-[var(--main-color)] font-[600] w-full text-center"
     >
       繼續購物
     </router-link>
 
     <router-link
       to="/home"
-      class="text-xl border-2 border-[var(--green-gray)] px-[48px] py-[24px] rounded-full text-[var(--green-gray)] font-[600]"
+      class="mt-[20px] text-xl border-2 border-[var(--green-gray)] px-[48px] py-[20px] rounded-full text-[var(--green-gray)] font-[600] w-full text-center"
     >
       前往首頁
     </router-link>
@@ -86,6 +88,14 @@
     再用md: 寫平板
     手機版 不用特別寫
   */
+
+  /* 
+    在F12 元素丟這段 找回滑鼠
+    const style = document.createElement('style');
+    style.innerHTML = `* { cursor: auto !important; }`;
+    document.head.appendChild(style); 
+  */
+
   .SS {
     display: flex;
     align-items: center;

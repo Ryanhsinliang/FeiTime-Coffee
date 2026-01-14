b597d55faf81d2d704cdf2fd84cb01e2f4551b15
Sandy <place6273456@gmail.com>
2026-01-14 08:23:31 +0800

    style: 後台頁調整sibar和main版型比例

--- src/views/Admin/Admin.vue
+++ src/views/Admin/Admin.vue
@@ -3,11 +3,11 @@
 </script>
 <template>
   <section class="flex h-screen">
-    <aside>
+    <aside class="fixed top-0 left-0 bottom-0 w-64">
       <AdminSidebar />
     </aside>
 
-    <main>
+    <main class="w-full overflow-y-auto ml-64">
       <router-view />
     </main>
   </section>

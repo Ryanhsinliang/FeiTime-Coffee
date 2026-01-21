e26aa23e4e1438cfa9524c21c3ea0691962bb94b
RyanKuo <t5a2r0@gmail.com>
2026-01-21 17:24:20 +0800

    chore: 把run buile產出的js刪除，避免vercel打包

--- src/services/homePage/featuredProduct.js
+++
@@ -1,8 +0,0 @@
-import api from '@/services/api';
-/**
- * 取得精選產品列表
- */
-export const fetchFeaturedProducts = async () => {
-    const response = await api.get('/api/featured/products');
-    return response.data;
-};

19b51e7cbafbe3434df7fc6e6b24caa01cd2e95d
Sandy <place6273456@gmail.com>
2026-01-28 01:30:02 +0800

    style:sibar RWD調整

--- src/views/Admin/Admin.vue
+++ src/views/Admin/Admin.vue
@@ -1,15 +1,13 @@
-<script setup>
+<script setup lang="ts">
   import AdminSidebar from '@/components/AdminSidebar.vue';
-  import AdminHeader from '@/components/AdminHeader.vue';
 </script>
+
 <template>
   <section class="flex h-screen bg-[#f9f8f6]">
     <aside class="fixed top-0 left-0 bottom-0 w-64">
       <AdminSidebar />
     </aside>
-
-    <main class="w-full overflow-y-auto ml-64">
-      <!-- <AdminHeader /> -->
+    <main class="w-full overflow-y-auto lg:ml-64">
       <router-view />
     </main>
   </section>
